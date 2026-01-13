f2fe8b85ae20d65a2688b33a960a4985a81d902d
danomite666 <197134483+danomite666@users.noreply.github.com>
2026-01-13 05:48:33 -0700

    Add Properties folder

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tri Cast Video Converter")]
+[assembly: AssemblyDescription("Video Conversion Tool For The Tri Cast Android Smart Phone App.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tri Cast")]
+[assembly: AssemblyProduct("Tri_Cast_Video_Converter")]
+[assembly: AssemblyCopyright("Copyright ©  2026 Tri Cast. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2c2b03f-607a-468f-86f3-23b6e403c8d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
